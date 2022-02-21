fdb4dc0b866d619e358c9c67072e07288afa6753
luisruarios <luis.rua@guarapo.dev>
2022-02-21 15:58:54 -0500

    solve-excercise

---
+++ src/store/amount/action.js
@@ -0,0 +1,16 @@
+const deposit = () => {
+    return {
+        type: "DEPOSIT_MONEY",
+        payload: 10,
+    }
+}
+
+const withdraw = () => {
+    return {
+        type: "WITHDRAW_MONEY",
+        payload: -10,
+    }
+}
+
+export {deposit, withdraw};
+ 
